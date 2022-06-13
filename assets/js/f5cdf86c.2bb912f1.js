4d7e44ba912509ef5ae2adc5478429232296f8d8
revvy02 <revvymail@gmail.com>
2022-06-13 17:30:16 -0500

    Built and published by Moonwave

---
+++ assets/js/f5cdf86c.2bb912f1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[110],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/Card","label":"\u200bCard"},{"type":"link","href":"/api/Keeper","label":"\u200bKeeper"},{"type":"link","href":"/api/Store","label":"\u200bStore"}]')}}]);
